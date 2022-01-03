6228e649edc13cd67152a090834ba48006f9934f
dee-na7721 <deenasitikhu@gmail.com>
2022-01-03 13:46:48 +0545

    My Profile Created

---
+++ resources/views/admin/employee/profile.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.hr.app')
+
+@section('title','Profile')
+
+@section('content')
+
+
+<!-- page title start -->
+<section class="my-3 pt-3">
+    <div class="text-center">
+        <h1 class="fs-2 title">My Profile</h1>
+    </div>
+    <div class="underline mx-auto"></div>
+</section>
+<!-- page title end -->
+
+<!-- form start -->
+<section class="form_container mx-auto">
+    <div class="row mx-auto">
+        <div class="col-md-2 col-sm-4 mb-4 mx-auto">
+            <img src="/assets/images/image.png" class="img-thumbnail img-fluid" width="100%">
+        </div>
+
+        <div class="col-md-10 col-sm-8" style="background-color:aliceblue; padding: 20px 40px;">
+            <form class="main_form p-4" enctype='multipart/form-data'>  
+                @include('admin.employee.profile_form')
+            </form>
+        </div>
+    </div>
+</section>
+<!-- form end -->
+@endsection
