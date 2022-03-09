06d41fb10af1b545271c80ec85d3443fffad07e9
dee-na7721 <deenasitikhu@gmail.com>
2022-03-09 22:23:22 +0545

    update in late punch in mail

--- resources/views/layouts/email/latePunchIn.blade.php
+++ resources/views/layouts/email/latePunchIn.blade.php
@@ -1,6 +1,6 @@
 <div>
     <p>Dear {{$name}},</p>
-    <p>You have punched in at <b>{{ $time }}</b>. Your punch-in will be logged as late, and the system will keep track of any subsequent late punch-ins.</p>
+    <p>You have punched in at <b>{{ $time }}</b>. Your punch-in is marked late, and the system is keeping track of any subsequent late punch-ins. Please punch in on time.</p>
     <p>Thank you,</p>
     --<br>
     HR | <span style="color:#0b5394;"><b>DRM SYSTEM</b></span> | Deerwalk Education Group, Sifal, Kathmandu, Nepal.<br>
