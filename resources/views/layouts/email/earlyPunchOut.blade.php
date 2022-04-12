281f6d62e8955c837d7cc9cea5280a9303e5f6e3
dee-na7721 <deenasitikhu@gmail.com>
2022-04-12 11:35:54 +0545

    Early Punch Out Mail Setting Created

---
+++ resources/views/layouts/email/earlyPunchOut.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <p>Hello {{ $employee }}</p>
+    <p>You have punched out early today i.e. at {{ date('Y-m-d H:i:s') }}. So, your half leave will be deducted from today by the DRM System.
+       <br><br> If you have any queries, please contact HR.
+    </p>
+    <p>Regards,</p>
+    <br>
+    <div id="signature">
+        --<br>
+        HR | <span style="color:#0b5394;"><b>DRM SYSTEM</b></span><br>
+        Deerwalk Education Group<br>
+        Sifal, Kathmandu<br>
+        Nepal<br>
+        <a href="deerwalk.edu.np">deerwalk.edu.np</a>
+        <br>
+        <p style="color:888888; font-family: ui-monospace;">
+            DISCLAIMER:<br>
+            This is an automatically generated email - please do not reply to it. If you have any queries please contact HR.
+        </p>
+    </div>
+</div>
