c9e993bf95d528136df8174d0f45e54a1c86450b
DWIT R & D <rnd@deerwalk.edu.np>
2022-04-13 17:00:42 +0545

    Debugging server write issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,5 +30,16 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production')) {
             // \URL::forceScheme('https');
         }
+
+        /**
+            * Somehow PHP is not able to write in default /tmp directory and SwiftMailer was failing.
+            * To overcome this situation, we set the TMPDIR environment variable to a new value.
+        */
+        
+        if (class_exists('Swift_Preferences')) {
+            \Swift_Preferences::getInstance()->setTempDir(storage_path().'/tmp');
+        } else {
+            \Log::warning('Class Swift_Preferences does not exists');
+        }
     }
 }
