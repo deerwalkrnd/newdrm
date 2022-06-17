20a908404531112ac722d1e59818999d12e32d0f
dee-na7721 <deenasitikhu@gmail.com>
2022-06-17 14:12:14 +0545

    perosonal force leave visibility updated

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -457,8 +457,7 @@ class LeaveRequestController extends Controller
     public function getMyForcedLeave()
     {
         $leaveList = LeaveRequest::where('employee_id',\Auth::user()->employee_id)
-                                    ->where('reason','Forced (System)')
-                                    ->orWhere('reason','Forced (System) Missed Punch Out')
+                                    ->where('reason','LIKE','Forced (System)%')
                                     ->orderBy('end_date','desc')
                                     ->paginate(20);
         return view('admin.leaveRequest.myForcedLeave')->with(compact('leaveList'));
