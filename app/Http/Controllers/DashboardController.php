58e85a01fafffcfce04eed79de31b47d5f6e518b
dee-na7721 <deenasitikhu@gmail.com>
2022-08-18 12:52:55 +0545

    birthday notification wihtout image added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -311,7 +311,7 @@ class DashboardController extends Controller
         $birthdayList = Employee::select('first_name','last_name','middle_name','date_of_birth')
                                 ->where('contract_status','active')
                                 ->where(function($query) use($curr_month,$curr_day){
-                                    $query->whereMonth('date_of_birth','>=',$curr_month)
+                                    $query->whereMonth('date_of_birth','>',$curr_month)
                                             ->orWhere(function($query) use($curr_month,$curr_day){
                                                 $query->whereMonth('date_of_birth',$curr_month)
                                                     ->whereDay('date_of_birth','>=',$curr_day);
