77a0416d79206ca2a2f6271a2db7d840aaaafdce
Tushar Luitel <tusharluitel88@gmail.com>
2024-01-22 17:48:10 +0545

    Update(Added hr to name):Added aakhancha thapa to hr mail

--- app/Helpers/MailHelper.php
+++ app/Helpers/MailHelper.php
@@ -41,7 +41,7 @@ class MailHelper{
         //     return $arr->employee->email;
         // })->toArray();
 
-        $hr = ['bijaya.shrestha@sifal.deerwalk.edu.np','hitesh.karki@deerwalk.edu.np','alisha.thapa@deerwalk.edu.np'];
+        $hr = ['bijaya.shrestha@sifal.deerwalk.edu.np','hitesh.karki@deerwalk.edu.np','aakancha.thapa@deerwalk.edu.np','alisha.thapa@deerwalk.edu.np'];
         
         return $hr;
     }
